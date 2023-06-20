15a52cce5456fea7303b8a6901d97d4e3d9b8eaa
StasGushchin <stas031094@gmail.com>
2023-06-20 14:07:32 +0300

    Hw-5 6 task is done

--- JS/hw-5/scriptHw-5.js
+++ JS/hw-5/scriptHw-5.js
@@ -76,6 +76,31 @@ console.log(numbs("f","a"));
 console.log(numbs(2,5));
 console.log(numbs(5,"a"));
 
+// 6 //
+let cube = (n) => {
+    if (isNaN(n) === false) {
+        let result = n**3;
+        return (`${n} в кубе равняется ${result}`)
+    } else {
+        return ('Переданный параметр не является числом')
+    }
+}
+
+console.log(cube(0));
+console.log(cube(1));
+console.log(cube(2));
+console.log(cube(3));
+console.log(cube(4));
+console.log(cube(5));
+console.log(cube(6));
+console.log(cube(7));
+console.log(cube(8));
+console.log(cube(9));
+console.log(cube(10));
+
+
+
+
 
 
 

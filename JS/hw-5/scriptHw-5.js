7bace4bc991649b93c73e535fc9d429cb6041735
StasGushchin <stas031094@gmail.com>
2023-06-21 14:34:13 +0300

    Hw-5 is done

--- JS/hw-5/scriptHw-5.js
+++ JS/hw-5/scriptHw-5.js
@@ -128,23 +128,23 @@ console.log(circle2.getPerimeter());
 
 // 8 //
 function seasons () {
-    let answer = prompt ('Введите номер месяца');
+    let answer = +prompt ('Введите номер месяца');
 
     if (isNaN(answer) === false) {
         
-        if (answer === 12||1||2) {
+        if (answer === 12 || answer === 1 || answer === 2) {
             alert('Зима');
         } 
         
-        else if (answer === 3||4||5) {
+        else if (answer === 3 || answer === 4 || answer === 5) {
             alert ('Весна');
         }
 
-        else if (answer === 6||7||8) {
+        else if (answer === 6 || answer === 7 || answer === 8) {
             alert ('Лето');
         }
 
-        else if (answer === 9||10||11) {
+        else if (answer === 9 || answer === 10 || answer === 11) {
             alert ('Осень');
         }
     
